b5c5e7c4de9097811b97754f6a0309b2bf75171f
redtower <redtower@gmail.com>
2011-01-17 16:29:44 +0900

    color-theme,clmemo.el,howmのプラグインをローカルに追加。それに伴う.emacsの修正。.emacsを.emacs.elに変更

---
+++ emacs.el
@@ -0,0 +1,138 @@
+;=======================================================================
+; load path
+;=======================================================================
+;(setq load-path
+;      (append
+;       (list (expand-file-name "~/.emacs/site-lisp")) load-path))
+
+;=======================================================================
+; color-theme
+;=======================================================================
+(add-to-list 'load-path "~/.emacs/site-lisp/color-theme/")
+
+(when window-system
+  (require 'color-theme)
+  (color-theme-initialize)
+  ;(color-theme-aliceblue)   ; Alice Blue
+  ;(color-theme-arjen)       ; Arjen
+  (color-theme-ramangalahy) ; Ramangalahy
+)
+
+;=======================================================================
+; misc
+;=======================================================================
+(mouse-wheel-mode 1)                            ; ホイールマウス
+(menu-bar-mode 0)                               ; メニューバー表示有無
+                                                ;     （0:表示しない、1:表示する）
+(if window-system                               ; ツールバー表示有無
+    (tool-bar-mode 0))                          ;     （0:表示しない、1:表示する）
+(global-font-lock-mode t)                       ; 文字の色つけ
+(setq line-number-mode t)                       ; カーソルのある行番号を表示
+(auto-compression-mode t)                       ; 日本語infoの文字化け防止
+(if window-system
+    (set-scroll-bar-mode 'right))               ; スクロールバーを右に表示
+(global-set-key "\C-z" 'undo)                   ; undo
+(setq-default indent-tabs-mode nil)             ; ソフトタブ
+(setq-default tab-width 4)                      ; TAB幅=4
+(setq default-tab-width 4)                      ; TAB幅=4
+(setq tab-stop-list                             ; タブ幅の倍数を設定
+ '(  4   8  12  16  20  24  28  32  36  40
+    44  48  52  56  60  64  68  72  76  80
+    84  88  92  96 100 104 108 112 116 120))
+(setq make-backup-files nil)                    ; バックアップファイルを作らない
+(setq delete-auto-save-files t)                 ; 終了時にオートセーブファイルを消す
+(global-set-key "\C-h" 'backward-delete-char)   ; ctrl-hでバックスペース
+(global-unset-key "\C-x\C-u")                   ; C-x C-u が何もしないように変更する
+                                                ;     （undo の typo 時誤動作防止）
+(when
+  (boundp 'show-trailing-whitespace)
+  (setq-default show-trailing-whitespace t))    ; 行末のスペースを強調表示
+
+;=======================================================================
+; クリップボードと共有する
+;       ref. http://d.hatena.ne.jp/x68kace/20080317/p3
+;=======================================================================
+(global-unset-key "\C-y")
+(global-set-key   "\C-y" 'clipboard-yank)
+(global-unset-key "\C-w")
+(global-set-key   "\C-w" 'clipboard-kill-region)
+(global-unset-key "\M-w")
+(global-set-key   "\M-w" 'clipboard-kill-ring-save)
+
+;=======================================================================
+; バッファリストの置き換え（bs）
+;=======================================================================
+(global-set-key [?\C-,]    'bs-cycle-next)
+(global-set-key [?\C-.]    'bs-cycle-previous)
+(global-set-key "\C-x\C-b" 'bs-show)
+
+;=======================================================================
+; Symbolic link ファイル編集時に yes/no を聞かないようにす
+;=======================================================================
+(custom-set-variables '(vc-follow-symlinks t))
+
+;=======================================================================
+; howm
+;=======================================================================
+(add-to-list 'load-path "~/.emacs/site-lisp/howm/")
+;;
+;; howm
+(setq howm-menu-lang 'ja)
+(global-set-key "\C-c,," 'howm-menu)
+
+(mapc
+ (lambda (f)
+   (autoload f
+     "howm" "Hitori Otegaru Wiki Modoki" t))
+ '(howm-menu howm-list-all howm-list-recent
+             howm-list-grep howm-create
+             howm-keyword-to-kill-ring))
+;; リンクを TAB で辿る
+(eval-after-load "howm-mode"
+  '(progn
+     (define-key howm-mode-map [tab] 'action-lock-goto-next-link)
+     (define-key howm-mode-map [(meta tab)] 'action-lock-goto-previous-link)))
+;; 「最近のメモ」一覧時にタイトル表示
+(setq howm-list-recent-title t)
+;; 全メモ一覧時にタイトル表示
+(setq howm-list-all-title t)
+;; メニューを 2 時間キャッシュ
+(setq howm-menu-expiry-hours 2)
+
+;; howm の時は auto-fill で
+(add-hook 'howm-mode-on-hook 'auto-fill-mode)
+
+;; RET でファイルを開く際, 一覧バッファを消す
+;; C-u RET なら残る
+(setq howm-view-summary-persistent nil)
+
+;; メニューの予定表の表示範囲
+;; 7 日前から
+(setq howm-menu-schedule-days 7)
+;; 3 日後まで
+(setq howm-menu-schedule-days-before 3)
+
+;; howm のファイル名
+;; 以下のスタイルのうちどれかを選んでください
+;; で，不要な行は削除してください
+;; 1 メモ 1 ファイル (デフォルト)
+(setq howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.howm")
+;; 1 日 1 ファイルであれば
+;;(setq howm-file-name-format "%Y/%m/%Y-%m-%d.howm")
+
+(setq howm-view-grep-parse-line
+      "^\\(\\([a-zA-Z]:/\\)?[^:]*\\.howm\\):\\([0-9]*\\):\\(.*\\)$")
+;; 検索しないファイルの正規表現
+(setq
+ howm-excluded-file-regexp
+ "/\\.#\\|[~#]$\\|\\.bak$\\|/CVS/\\|\\.doc$\\|\\.pdf$\\|\\.ppt$\\|\\.xls$")
+
+;=======================================================================
+; clmemo.el
+;=======================================================================
+(add-to-list 'load-path "~/.emacs/site-lisp/clmemo/")
+;;
+;; clmemo.el
+(autoload 'clmemo "clmemo" "ChangeLog memo mode." t)
+(setq clmemo-file-name "~/clmemo.txt")
+(global-set-key "\C-xM" 'clmemo)
