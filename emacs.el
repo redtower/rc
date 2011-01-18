c9addfeadbb52d77ba38f4215198a07e8be368cb
redtower <redtower@gmail.com>
2011-01-18 11:24:23 +0900

    特にhowmで使う日付入力の処理を追加

--- emacs.el
+++ emacs.el
@@ -129,6 +129,43 @@
      (buffer-file-name (current-buffer)))))
 
 ;=======================================================================
+; 日付入力 （M-x calendar）
+;     ref. http://www.bookshelf.jp/soft/meadow_38.html#SEC563
+;=======================================================================
+(eval-after-load "calendar"
+  '(progn
+     (define-key calendar-mode-map
+       "\C-m" 'my-insert-day)
+     (defun my-insert-day ()
+       (interactive)
+       (let ((day nil)
+             (calendar-date-display-form
+         '("[" year "-" (format "%02d" (string-to-int month))
+           "-" (format "%02d" (string-to-int day)) "]")))
+         (setq day (calendar-date-string
+                    (calendar-cursor-to-date t)))
+         (exit-calendar)
+         (insert day)))))
+
+;=======================================================================
+; 日付簡易入力 （C-c C-d, C-c C-t, C-c Esc d）
+;=======================================================================
+(defun my-get-date-gen (form)
+  (insert (format-time-string form)))
+(defun my-get-date ()
+  (interactive)
+  (my-get-date-gen "[%Y-%m-%d]"))
+(defun my-get-time ()
+  (interactive)
+  (my-get-date-gen "[%H:%M]"))
+(defun my-get-dtime ()
+  (interactive)
+  (my-get-date-gen "[%Y-%m-%d %H:%M]"))
+(global-set-key "\C-c\C-d" 'my-get-date)
+(global-set-key "\C-c\C-t" 'my-get-time)
+(global-set-key "\C-c\ed" 'my-get-dtime)
+
+;=======================================================================
 ; clmemo.el
 ;=======================================================================
 (add-to-list 'load-path "~/.emacs/site-lisp/clmemo/")
