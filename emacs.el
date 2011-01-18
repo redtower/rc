7f97f10636e1b8590c255ce096d50c21a51519de
redtower <redtower@gmail.com>
2011-01-18 11:23:14 +0900

    C-c C-c で現バッファの内容を保存してバッファを消す処理を追加

--- emacs.el
+++ emacs.el
@@ -73,6 +73,17 @@
 (custom-set-variables '(vc-follow-symlinks t))
 
 ;=======================================================================
+; C-c C-c ： 現バッファの内容を保存してバッファを消す
+;     ref. http://howm.sourceforge.jp/cgi-bin/hiki/hiki.cgi?SaveAndKillBuffer
+;=======================================================================
+(defun my-save-and-kill-buffer ()
+  (interactive)
+  (save-buffer)
+  (kill-buffer nil))
+
+(global-set-key "\C-c\C-c" 'my-save-and-kill-buffer)
+
+;=======================================================================
 ; howm
 ;=======================================================================
 (add-to-list 'load-path "~/.emacs/site-lisp/howm/")
