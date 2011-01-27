430bff22b3b5904984e7521bfe0eac7a438d44a1
redtower <redtower@gmail.com>
2011-01-28 00:58:30 +0900

    ファイルにメモを残すipsの設定を追加

--- emacs.el
+++ emacs.el
@@ -22,8 +22,19 @@
 ;=======================================================================
 (add-to-list 'load-path "~/.emacs/site-lisp/anything/")
 (require 'anything-startup)
-
 (global-set-key "\C-x\C-b" 'anything-for-files)
+
+;=======================================================================
+; ファイルにメモを残す（ips + anything-ipa）
+;     M-x     ipa-insert   メモを作成
+;     M-x     ipa-edit     カーソルより後のメモを編集
+;     C-u M-x ipa-edit     カソルより前のメモを編集
+;     M-x     ipa-move     メモを移動
+;     M-x     anything-ipa anything で現在のバッファのメモをリスト。
+;                          TAB でジャンプ。
+;=======================================================================
+(require 'ipa)
+(require 'anything-ipa)
 
 ;=======================================================================
 ; color-theme
