978af66b60dc11194af0f858c51e2fb3644cbb0c
redtower <redtower@gmail.com>
2011-01-29 18:00:48 +0900

    表示揃え

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -432,7 +432,7 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/zencoding-mode/")
 (require 'zencoding-mode) 
 (add-hook 'sgml-mode-hook 'zencoding-mode) ; html-modeとかで自動出来にzencodingできるようにする
-(define-key zencoding-mode-keymap (kbd "C-c C-n") 'zencoding-expand-line)
+(define-key zencoding-mode-keymap    (kbd "C-c C-n") 'zencoding-expand-line)
 (define-key zencoding-preview-keymap (kbd "C-c C-n") 'zencoding-preview-accept)
 
 ;=======================================================================
