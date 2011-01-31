a0737b4add14a7064dece708fa0a42b4c3d375bd
redtower <redtower@gmail.com>
2011-01-31 18:04:18 +0900

    sh設定変更

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -507,11 +507,13 @@
 
 (cond ((is_windows)     ; NTEmacs
        (shell-pop-set-internal-mode "eshell"))
+       (shell-pop-set-internal-mode-shell "bash")
       ((is_mac)         ; Mac
        (shell-pop-set-internal-mode "ansi-term")
 ;       (shell-pop-set-internal-mode "eshell")
+       (shell-pop-set-internal-mode-shell "/bin/zsh")
       ))
-(shell-pop-set-internal-mode-shell "/bin/zsh")
+
 (shell-pop-set-window-height 60)
 
 ;=======================================================================
