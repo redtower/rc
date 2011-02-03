7ad6075c4137d3e8f076b46813f1b1e7851b1993
redtower <redtower@gmail.com>
2011-02-03 23:56:58 +0900

    プラグインのemacs-w3m,japanese-holidays.el,calfw.elを追加

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -657,6 +657,85 @@
 ;;=======================================================================
 (add-to-list 'load-path "~/.emacs.d/elisp/navi2ch/share/emacs/site-lisp/")
 (autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t)
+(setq navi2ch-article-auto-range nil)               ; 最初に全件表示する(デフォルトは100)
+
+;;=======================================================================
+;; @ emacs-w3m
+;;=======================================================================
+(add-to-list 'load-path "~/.emacs.d/elisp/emacs-w3m/share/emacs/site-lisp/w3m/")
+(require 'w3m-load)
+(setq browse-url-browser-function 'w3m-browse-url)  ; ブラウザをw3mにする
+(global-set-key "\C-xm" 'browse-url-at-point)       ; カーソル位置の文字列をURLとしてブラザを起動する
+
+;;=======================================================================
+;; @ japanese-holidays.el
+;;=======================================================================
+(add-to-list 'load-path "~/.emacs.d/elisp/japanese-holidays")
+(add-hook 'calendar-load-hook
+          (lambda ()
+            (require 'japanese-holidays)
+            (setq calendar-holidays
+                  (append japanese-holidays local-holidays other-holidays))))
+
+;;=======================================================================
+;; @ calfw.el
+;;   ref. http://d.hatena.ne.jp/kiwanami/?20110107215552
+;;=======================================================================
+(add-to-list 'load-path "~/.emacs.d/elisp/calfw")
+(require 'calfw)
+(global-set-key "\C-cl"  'cfw:open-calendar-buffer)
+(defvar my-howm-schedule-page "2011年予定") ; 予定を入れるメモのタイトル
+(defun my-cfw-open-schedule-buffer ()
+  (interactive)
+  (let* 
+      ((date (cfw:cursor-to-nearest-date))
+       (howm-items 
+        (howm-folder-grep
+         howm-directory
+         (regexp-quote my-howm-schedule-page))))
+    (cond
+     ((null howm-items) ; create
+      (howm-create-file-with-title my-howm-schedule-page nil nil nil nil))
+     (t
+      (howm-view-open-item (car howm-items))))
+    (goto-char (point-max))
+    (unless (bolp) (insert "\n"))
+    (insert
+     (format "[%04d-%02d-%02d]@ "
+             (calendar-extract-year date)
+             (calendar-extract-month date)
+             (calendar-extract-day date)))))
+;;=======================================================================
+;; @ カレンダー表記
+;;=======================================================================
+(setq calendar-month-name-array    ;; 月
+      ["January" "February" "March"     "April"   "May"      "June"
+       "July"    "August"   "September" "October" "November" "December"])
+(setq calendar-day-name-array      ;; 曜日
+      ["Sun" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat"])
+
+;;=======================================================================
+;; @ calfw <-> howm 連携
+;;=======================================================================
+(eval-after-load "howm-menu"         ; calfw <-> howm 連携
+  '(progn
+     (require 'calfw-howm)
+     (cfw:install-howm-schedules)
+     (define-key howm-mode-map (kbd "M-C") 'cfw:elscreen-open-howm-calendar)
+     (define-key cfw:howm-schedule-map (kbd "i") 'my-cfw-open-schedule-buffer)
+     (define-key cfw:howm-schedule-inline-keymap (kbd "i") 'my-cfw-open-schedule-buffer)
+     ))
+
+(setq cfw:howm-schedule-summary-transformer 
+  (lambda (line) (split-string (replace-regexp-in-string "^[^@!]+[@!] " "" line) " / ")))
+
+;;=======================================================================
+;; @ calfw <-> ical（Google Calendar） 連携
+;;   Google Calendar の iCalの指定は、~/rc/private/emacs.el
+;;=======================================================================
+;(require 'calfw-ical)
+;(cfw:install-ical-schedules)
+;;(setq cfw:ical-calendar-contents-sources '("http://www.google.com/calendar/ical/～.ics"))
 
 ;=======================================================================
 ; load private-emacs.el
