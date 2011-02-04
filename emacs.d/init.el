88500bbae3a9e864ae1aba38782a3b5a04165ef8
redtower <redtower@gmail.com>
2011-02-04 11:54:58 +0900

    wp-emacs（weblogger）の設定を強化（C-x C-sのキーバインドを除去、C-c C-cでポスト時にバッファを消去しないようにする、C-c C-kでエントリーを削除しないようにする、zencoding-modeになるように設定、自動改行をOFF） ref. http://my.opera.com/ymirlin/blog/2010/10/17/weblogger-el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -495,11 +495,38 @@
 (setq cua-enable-cua-keys nil)
 
 ;;=======================================================================
-;; @ wp-emacs
+;; @ wp-emacs（weblogger）
 ;;=======================================================================
 (add-to-list 'load-path "~/.emacs.d/elisp/wp-emacs/")
 (require 'weblogger)
-(global-set-key "\C-c\C-w" 'weblogger-start-entry)
+(global-set-key "\C-c\C-w" 'weblogger-start-entry)  ; weblogger起動（C-c C-w）
+
+(defun my-weblogger-send-entry (&optional arg)
+  (interactive)
+  (save-buffer)
+  (set-buffer-modified-p t)
+  (weblogger-save-entry nil arg)
+  (my-weblogger-quit))
+
+(defun my-weblogger-quit ()
+  (interactive)
+  (when (y-or-n-p "Do you want to quit weblogger-entry? ")
+    (bury-buffer)))
+
+(add-hook 'weblogger-start-edit-entry-hook
+          '(lambda ()
+             ;; 必要ないキーバインドを除去
+             ;; (define-key weblogger-entry-mode-map (kbd "C-c C-k") nil)
+             (define-key weblogger-entry-mode-map (kbd "C-x C-s") nil)
+             ;; (define-key weblogger-entry-mode-map (kbd "C-c C-n") nil)
+             ;; (define-key weblogger-entry-mode-map (kbd "C-c C-p") nil)
+
+             (define-key weblogger-entry-mode-map
+               (kbd "C-c C-c") 'my-weblogger-send-entry)
+             (define-key weblogger-entry-mode-map
+               (kbd "C-c C-k") 'my-weblogger-quit)
+             (zencoding-mode t)                     ; zencoding-mode
+             (auto-fill-mode -1)))                  ; 自動改行をOFF
 
 ;;=======================================================================
 ;; @ タグジャンプ（etags M-. M-*）
