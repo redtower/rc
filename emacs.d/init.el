5182dd4c627e82db554a4b9a6afd4a7b14bb4f13
redtower <redtower@gmail.com>
2011-02-04 11:58:58 +0900

    ttp で始まる URL を http として認識させる。 ref. http://www.bookshelf.jp/soft/meadow_55.html#SEC829

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -727,6 +727,41 @@
 (global-set-key "\C-xm" 'browse-url-at-point)       ; カーソル位置の文字列をURLとしてブラザを起動する
 
 ;;=======================================================================
+;; @ ttp で始まる URL を http として認識させる。
+;;   ref. http://www.bookshelf.jp/soft/meadow_55.html#SEC829
+;;=======================================================================
+;; for browse-url-at-mouse
+(setq thing-at-point-url-regexp
+      (concat
+       "\\<\\(h?ttps?://\\|ftp://\\|gopher://\\|telnet://"
+       "\\|wais://\\|file:/\\|s?news:\\|mailto:\\)"
+       thing-at-point-url-path-regexp))
+(defadvice thing-at-point-url-at-point (after support-omitted-h activate)
+  (when (and ad-return-value (string-match "\\`ttps?://" ad-return-value))
+    (setq ad-return-value (concat "h" ad-return-value))))
+
+;; for emacs-w3m
+(setq ffap-url-regexp
+      (concat
+       "\\`\\("
+       "news\\(post\\)?:\\|mailto:\\|file:"
+       "\\|"
+       "\\(ftp\\|h?ttps?\\|telnet\\|gopher\\|www\\|wais\\)://"
+       "\\)."))
+(defadvice ffap-url-at-point (after support-omitted-h activate)
+  (when (and ad-return-value (string-match "\\`ttps?://" ad-return-value))
+    (setq ad-return-value (concat "h" ad-return-value))))
+
+;; for SEMI
+(setq mime-browse-url-regexp
+      (concat "\\(h?ttps?\\|ftp\\|file\\|gopher\\|news\\|telnet\\|wais\\|mailto\\):"
+              "\\(//[-a-zA-Z0-9_.]+:[0-9]*\\)?"
+              "[-a-zA-Z0-9_=?#$@~`%&*+|\\/.,]*[-a-zA-Z0-9_=#$@~`%&*+|\\/]"))
+(defadvice browse-url (before support-omitted-h (url &rest args) activate)
+  (when (and url (string-match "\\`ttps?://" url))
+    (setq url (concat "h" url))))
+
+;;=======================================================================
 ;; @ japanese-holidays.el
 ;;=======================================================================
 (add-to-list 'load-path "~/.emacs.d/elisp/japanese-holidays")
