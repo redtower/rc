231d443a05babdd74e14526aa73e03fea7d340e6
redtower <redtower@gmail.com>
2011-02-07 18:25:28 +0900

    yasnippetの設定強化（キーバインドC-x iに変更、個人的なsnippetsを~/.emacs.d/my-snippetsに出力するように変更）

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -689,8 +689,29 @@
 ;;=======================================================================
 (add-to-list 'load-path "~/.emacs.d/elisp/yasnippet/")
 (require 'yasnippet)
+
+;;(setq yas/trigger-key "SPC")
+(setq yas/trigger-key nil)
+(setq yas/next-field-key "TAB")
+(setq yas/prev-field-key "<S-tab>")
+
+(define-key yas/minor-mode-map (kbd "C-x i i") 'yas/insert-snippet)
+(define-key yas/minor-mode-map (kbd "C-x i f") 'yas/find-snippets)
+(define-key yas/minor-mode-map (kbd "C-x i n") 'yas/new-snippet)
+(define-key yas/minor-mode-map (kbd "C-x i v") 'yas/visit-snippet-file)
+(define-key yas/minor-mode-map (kbd "C-x i e") 'yas/expand)
+;; コメントやリテラルではスニペットを展開しない
+(setq yas/buffer-local-condition
+      '(or (not (or (string= "font-lock-comment-face"
+                             (get-char-property (point) 'face))
+                    (string= "font-lock-string-face"
+                             (get-char-property (point) 'face))))
+           '(require-snippet-condition . force-in-comment)))
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/elisp/yasnippet/snippets")
+(setq yas/root-directory '("~/.emacs.d/my-snippets"
+                           "~/.emacs.d/elisp/yasnippet/snippets"))
+;; Load the snippets
+(mapc 'yas/load-directory yas/root-directory)
 
 ;;=======================================================================
 ;; @ navi2ch
