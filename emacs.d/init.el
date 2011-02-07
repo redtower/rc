d63b101657d7f356c9ed4149e7823484ab315187
redtower <redtower@gmail.com>
2011-02-07 18:27:21 +0900

    Mew,yenを追加

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -883,12 +883,44 @@
         hs-c-like-adjust-block-beginning)
       hs-special-modes-alist)
 
-;=======================================================================
-; load private-emacs.el
-;=======================================================================
-(load "~/rc/private/emacs.el" :if-does-not-exist nil)
+;;=======================================================================
+;; @ Mew
+;;=======================================================================
+(cond
+ ((is_windows)
+  (add-to-list 'load-path "~/.emacs.d/elisp/mew")
+  (autoload 'mew "mew" nil t)
+  (autoload 'mew-send "mew" nil t)
+
+  ;; Optional setup (Read Mail menu):
+  (setq read-mail-command 'mew)
+
+  ;; Optional setup (e.g. C-xm for sending a message):
+  (autoload 'mew-user-agent-compose "mew" nil t)
+  (if (boundp 'mail-user-agent)
+      (setq mail-user-agent 'mew-user-agent))
+  (if (fboundp 'define-mail-user-agent)
+      (define-mail-user-agent
+        'mew-user-agent
+        'mew-user-agent-compose
+        'mew-draft-send-message
+        'mew-draft-kill
+        'mew-send-hook))
+
+  (setq mew-use-cached-passwd t)))
 
 ;;=======================================================================
+;; @ yen
+;;=======================================================================
+(add-to-list 'load-path "~/.emacs.d/elisp/yen")
+(require 'yen)
+
+;;=======================================================================
+;; @ load private-emacs.el
+;;=======================================================================
+(load "~/rc/private/emacs.el" :if-does-not-exist nil)
+
+;;======================================================================
 ;; @ ホームディレクトリに移動する
 ;;=======================================================================
 (cd "~")
