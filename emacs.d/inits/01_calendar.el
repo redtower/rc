b57d0219f7d9e12fe124658874fb4f53963a4572
redtower <redtower@gmail.com>
2011-02-09 20:55:31 +0900

    ロードパスの最後にスラッシュが付いてたり付いてなかったりするので全て削除、カレンダーにjapanese-holidaysが反映されていなかったので先に読み込むように変更、カレンダーに土日の強調表示追加

--- emacs.d/inits/01_calendar.el
+++ emacs.d/inits/01_calendar.el
@@ -1,4 +1,39 @@
 ;;=======================================================================
+;; @ japanese-holidays.el
+;;=======================================================================
+(add-hook 'calendar-load-hook
+          (lambda ()
+            (require 'japanese-holidays)
+            (setq calendar-holidays
+                  (append japanese-holidays local-holidays other-holidays))))
+
+;;=======================================================================
+;; @ calendar
+;;   ref. http://www.heimat.gr.jp/pipermail/elips/2000/000238.html
+;;=======================================================================
+; 祝祭日に印を付ける。
+(setq mark-holidays-in-calendar t)
+; 現在日付に印をつける。
+(add-hook 'today-visible-calendar-hook   'calendar-mark-today)
+
+;; 週末（weekend）に色を付ける。
+(setq calendar-weekend-marker 'diary)
+(add-hook 'today-visible-calendar-hook   'calendar-mark-weekend)
+(add-hook 'today-invisible-calendar-hook 'calendar-mark-weekend)
+; 週末を土日に設定する。
+(setq calendar-weekend '(0 6))
+
+;; 日曜日のフェイスを設定
+(make-face 'sunday-face)
+(set-face-foreground 'sunday-face "Tomato")
+;; 土曜日のフェイスを設定
+(make-face 'saturday-face)
+(set-face-foreground 'saturday-face "LightBlue")
+(setq calendar-sunday-marker   'sunday-face)
+(setq calendar-saturday-marker 'saturday-face)
+(setq diary-entry-marker (quote bold-italic))
+
+;;=======================================================================
 ;; @ 日付入力 （M-x calendar）
 ;;     ref. http://www.bookshelf.jp/soft/meadow_38.html#SEC563
 ;;=======================================================================
--- emacs.d/inits/30_japanese-holidays.el
+++
@@ -1,8 +0,0 @@
-;;=======================================================================
-;; @ japanese-holidays.el
-;;=======================================================================
-(add-hook 'calendar-load-hook
-          (lambda ()
-            (require 'japanese-holidays)
-            (setq calendar-holidays
-                  (append japanese-holidays local-holidays other-holidays))))
