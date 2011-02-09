77b8352790db1b47cef339ed350162a4b93cd3cd
redtower <redtower@gmail.com>
2011-02-09 21:01:26 +0900

    全角空白、Tab、改行表示の色付けを修正（背景色に合わせて変更する）

--- emacs.d/inits/30_jaspace.el
+++ emacs.d/inits/30_jaspace.el
@@ -1,8 +1,45 @@
 ;;=======================================================================
 ;; @ 全角空白、Tab、改行表示
 ;;=======================================================================
-(require 'jaspace)
-(setq jaspace-alternate-jaspace-string "□")
-;(setq jaspace-alternate-eol-string "↓\n")
-(setq jaspace-highlight-tabs t)
-(setq jaspace-modes(mapcar 'car my-fav-modes))
+(when (require 'jaspace nil t)
+  (when (boundp 'jaspace-modes)
+    (setq jaspace-modes (mapcar 'car my-fav-modes)))
+  (when (boundp 'jaspace-alternate-jaspace-string)
+    (setq jaspace-alternate-jaspace-string "□"))
+;  (when (boundp 'jaspace-alternate-eol-string)
+;    (setq jaspace-alternate-eol-string "↓\n"))
+  (when (boundp 'jaspace-highlight-tabs)
+    (setq jaspace-highlight-tabs ?^))
+  (add-hook 'jaspace-mode-off-hook
+            (lambda()
+              (when (boundp 'show-trailing-whitespace)
+                (setq show-trailing-whitespace nil))))
+  (add-hook 'jaspace-mode-hook
+            (lambda()
+              (progn
+                (when (boundp 'show-trailing-whitespace)
+                  (setq show-trailing-whitespace t))
+                (face-spec-set 'jaspace-highlight-jaspace-face  ; 全角空白
+                               '((((class color) (background light))
+                                  (:foreground "blue"))
+                                 (t (:foreground "green"))))
+                (face-spec-set 'jaspace-highlight-tab-face      ; タブ
+                               '((((class color) (background light))
+                                  (:foreground "red"
+                                   :background "unspecified"
+                                   :strike-through nil
+                                   :underline t))
+                                 (t (:foreground "purple"
+                                     :background "unspecified"
+                                     :strike-through nil
+                                     :underline t))))
+                (face-spec-set 'trailing-whitespace             ; 半角空白
+                               '((((class color) (background light))
+                                  (:foreground "red"
+                                   :background "unspecified"
+                                   :strike-through nil
+                                   :underline t))
+                                 (t (:foreground "purple"
+                                     :background "unspecified"
+                                     :strike-through nil
+                                     :underline t))))))))
