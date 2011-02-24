13aef8910d33d6360c5ed5ecbedc0b2a88dc8fa4
redtower <redtower@gmail.com>
2011-02-24 14:50:43 +0900

    clmemo.txtのファイルをchangelogに変更

--- emacs.d/inits/30_clmemo.el
+++ emacs.d/inits/30_clmemo.el
@@ -2,5 +2,6 @@
 ;; @ clmemo.el
 ;;=======================================================================
 (autoload 'clmemo "clmemo" "ChangeLog memo mode." t)
-(setq clmemo-file-name "~/rc/private/clmemo.txt")
+;(setq clmemo-file-name "~/rc/private/clmemo.txt")
+(setq clmemo-file-name "~/rc/private/changelog/changelog")
 (global-set-key "\C-xM" 'clmemo)
