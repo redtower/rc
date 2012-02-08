a8140be2b98353e0f84dfe75fefcfdabe0c6b41d
redtower <redtower@gmail.com>
2012-02-08 13:51:37 +0900

    Windows（Cygwin）でmarkdownコマンドが見つからなかったので指定する。

--- emacs.d/inits/50_markdown-mode.el
+++ emacs.d/inits/50_markdown-mode.el
@@ -5,3 +5,6 @@
    "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
    (cons '("\\.\\(md\\|markdown\\)" . markdown-mode) auto-mode-alist))
+
+;; markdown-modeで利用するコマンド
+(cond ((is_windows) (setq markdown-command "/usr/local/bin/markdown")))
