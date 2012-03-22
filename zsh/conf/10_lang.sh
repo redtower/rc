2620bbf505972d38166709916c74fa5bf3390d46
redtower <redtower@gmail.com>
2012-03-22 18:00:29 +0900

    LANG の指定に export を追加

--- zsh/conf/10_lang.sh
+++ zsh/conf/10_lang.sh
@@ -1,8 +1,8 @@
 # 文字コードの設定
 case $TERM in
-    linux) LANG=C ;;
-        *) LANG=ja_JP.UTF-8 ;;
+    linux) export LANG=C ;;
+        *) export LANG=ja_JP.UTF-8 ;;
 esac
 case ${UID} in
-	0) LANG=C ;; # root ユーザは LANG=C にする
+	0) export LANG=C ;; # root ユーザは LANG=C にする
 esac
