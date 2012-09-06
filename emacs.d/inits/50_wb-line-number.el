13ecf3a2bd96a67cc0b2d26feac07b3c1c59d607
redtower <redtower@gmail.com>
2012-09-06 15:50:33 +0900

    emacs に行番号を表示する wb-line-number.el を追加

---
+++ emacs.d/inits/50_wb-line-number.el
@@ -0,0 +1,8 @@
+;;=======================================================================
+;; @ wb-line-number
+;;=======================================================================
+(require 'wb-line-number)
+(setq truncate-partial-width-windows nil)
+(set-scroll-bar-mode nil)
+(setq wb-line-number-scroll-bar t)
+(wb-line-number-toggle)
