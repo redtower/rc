4750493ebd741982bd3b3be5728de4fb73c8a0d6
redtower <redtower@gmail.com>
2012-09-06 16:52:40 +0900

    emacs の行番号を wb-line-number.el から linum に変更

--- emacs.d/inits/30_linum.el
+++ emacs.d/inits/30_linum.el
@@ -4,5 +4,5 @@
 (require 'linum)
 (global-linum-mode t)                          ; バッファ中の行番号表示
 (set-face-attribute                            ; 行番号のフォーマット
- 'linum nil :foreground "red" :height 0.8)
+ 'linum nil :foreground "hotpink" :height 1.0)
 (setq linum-format "%4d")
