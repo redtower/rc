6848cbf1b94a88655cdfb9bedbdf727e67266061
redtower <redtower@gmail.com>
2012-09-19 11:43:18 +0900

    emacs: change mac's font

--- emacs.d/inits/01_font.el
+++ emacs.d/inits/01_font.el
@@ -8,23 +8,17 @@
     (cond
      ((eq system-type 'windows-nt)   ; NTEmacs
       (set-default-font "M+2VM+IPAG circle-12")
-      (set-fontset-font
-       (frame-parameter nil 'font)
-       'japanese-jisx0208
-       '("M+2VM+IPAG circle" . "unicode-bmp"))
+      (set-fontset-font (frame-parameter nil 'font)
+                        'japanese-jisx0208
+                        '("M+2VM+IPAG circle" . "unicode-bmp"))
       ; IME変換時フォントの設定（テストバージョンのみ）
       (setq w32-ime-font-face "MigMix 1M")
       (setq w32-ime-font-height 22))
      ((is_mac)                       ; Mac
-      (create-fontset-from-ascii-font
-       "Menlo-12:weight=normal:slant=normal" nil "menlokakugo")
-      (set-fontset-font
-       "fontset-menlokakugo"
-       'unicode
-       (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
-       nil
-       'append)
-      (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo")))
+      (set-default-font "Monaco-14")
+      (set-fontset-font (frame-parameter nil 'font)
+                        'japanese-jisx0208
+                        '("ヒラギノ角ゴ pro w3" . "jisx0208.1983")))
      (
       (set-default-font "Monospace-12")
       (set-fontset-font (frame-parameter nil 'font)
