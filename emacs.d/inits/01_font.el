95c33e9a5d6f4c775bc906667e72f1d3422fa770
redtower <redtower@gmail.com>
2013-02-11 15:23:19 +0900

    change emacs font for mac

--- emacs.d/inits/01_font.el
+++ emacs.d/inits/01_font.el
@@ -14,11 +14,16 @@
       ; IME変換時フォントの設定（テストバージョンのみ）
       (setq w32-ime-font-face "MigMix 1M")
       (setq w32-ime-font-height 22))
+
      ((is_mac)                       ; Mac
       (set-default-font "Monaco-14")
-      (set-fontset-font (frame-parameter nil 'font)
-                        'japanese-jisx0208
-                        '("ヒラギノ角ゴ pro w3" . "jisx0208.1983")))
+      (set-fontset-font nil
+                        'unicode
+                        (font-spec :family "IPAexゴシック")
+                        nil
+                        'append)
+     )
+
      (
       (set-default-font "Monospace-12")
       (set-fontset-font (frame-parameter nil 'font)
