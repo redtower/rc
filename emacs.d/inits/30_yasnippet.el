39948e6ae0dca6a1268fe232a08f08665108627c
redtower <redtower@gmail.com>
2013-02-23 22:09:02 +0900

    yasnippet

--- emacs.d/inits/30_yasnippet.el
+++ emacs.d/inits/30_yasnippet.el
@@ -9,11 +9,11 @@
 ;;(setq yas/next-field-key "TAB")
 ;;(setq yas/prev-field-key "<S-tab>")
 
-(define-key yas/minor-mode-map (kbd "C-x i i") 'yas/insert-snippet)
-(define-key yas/minor-mode-map (kbd "C-x i f") 'yas/find-snippets)
-(define-key yas/minor-mode-map (kbd "C-x i n") 'yas/new-snippet)
-(define-key yas/minor-mode-map (kbd "C-x i v") 'yas/visit-snippet-file)
-(define-key yas/minor-mode-map (kbd "C-x i e") 'yas/expand)
+;;(define-key yas/minor-mode-map (kbd "C-x i i") 'yas/insert-snippet)
+;;(define-key yas/minor-mode-map (kbd "C-x i f") 'yas/find-snippets)
+;;(define-key yas/minor-mode-map (kbd "C-x i n") 'yas/new-snippet)
+;;(define-key yas/minor-mode-map (kbd "C-x i v") 'yas/visit-snippet-file)
+;;(define-key yas/minor-mode-map (kbd "C-x i e") 'yas/expand)
 ;; コメントやリテラルではスニペットを展開しない
 (setq yas/buffer-local-condition
       '(or (not (or (string= "font-lock-comment-face"
@@ -21,7 +21,7 @@
                     (string= "font-lock-string-face"
                              (get-char-property (point) 'face))))
            '(require-snippet-condition . force-in-comment)))
-(yas/initialize)
+;;(yas/initialize)
 (setq yas/root-directory '("~/.emacs.d/my-snippets"
                            "~/.emacs.d/elisp/yasnippet/snippets"))
 ;; Load the snippets
