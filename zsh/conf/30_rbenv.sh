d48c7b505c61a88aca8f96f4229a9bc9f56ff79d
redtower <redtower@gmail.com>
2013-05-11 21:07:13 +0900

    rbenv があるかチェックするように修正

--- zsh/conf/30_rbenv.sh
+++ zsh/conf/30_rbenv.sh
@@ -1,6 +1,9 @@
 # rbenv
 # https://github.com/sstephenson/rbenv#section_2
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+R=`which rbenv`
+if [ $? -eq 0 ]; then
+    eval "$(rbenv init -)"
+fi
 
 # End of lines configured by zsh-newuser-install
