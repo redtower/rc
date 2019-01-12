1741928e73eca62483baa41aee06a759632e0af1
MORIUCHI Shinichiro <moiuchi@nsc-kk.co.jp>
2019-01-12 23:56:54 +0900

    del vimperator

--- vimperator/conf/50_hatenabookmark.js
+++
@@ -1,5 +0,0 @@
-" hatena bookmark "
-if (typeof hBookmark != 'undefined')
-   liberator.loadScript(
-        'chrome://hatenabookmark/content/vimperator/plugin/hatenabookmark.js',
-        {__proto__: this});
