d50a33c0349404803ccd40c8c0f6b9e1343e5d12
MORIUCHI Shinichiro <moiuchi@nsc-kk.co.jp>
2020-08-19 20:45:08 +0900

    端末起動時に自動でtmuxセッションに接続

---
+++ zsh/conf/60_tmux.sh
@@ -0,0 +1,26 @@
+
+function t()
+{
+  create_new_session="Create New Session"
+  ID="`tmux list-sessions`\n${create_new_session}:"
+
+  ID="`echo $ID | peco | cut -d: -f1`"
+  if [[ "$ID" = "${create_new_session}" ]]; then
+    tmux new-session
+  elif [[ -n "$ID" ]]; then
+    tmux attach-session -t "$ID"
+  else
+    :  # Start terminal normally
+  fi
+}
+
+if [[ ! -n $TMUX && $- == *l* ]]; then
+  # get the IDs
+  ID="`tmux list-sessions`"
+  if [[ -z "$ID" ]]; then
+    tmux new-session
+  fi
+
+  t
+fi
+
