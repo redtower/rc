40cf7df66a64b8fbabe1d8d56992b0a8bd2a771e
MORIUCHI Shinichiro <moiuchi@nsc-kk.co.jp>
2021-05-12 14:02:52 +0900

    tmuxで新規セッション作成時に画面分割する

--- zsh/conf/60_tmux.sh
+++ zsh/conf/60_tmux.sh
@@ -6,7 +6,8 @@ function t()
 
   ID="`echo $ID | peco | cut -d: -f1`"
   if [[ "$ID" = "${create_new_session}" ]]; then
-    tmux new-session
+    #tmux new-session
+    tmux new-session \; source-file ~/.tmux/new-session
   elif [[ -n "$ID" ]]; then
     tmux attach-session -t "$ID"
   else
@@ -18,7 +19,8 @@ if [[ ! -n $TMUX && $- == *l* ]]; then
   # get the IDs
   ID="`tmux list-sessions`"
   if [[ -z "$ID" ]]; then
-    tmux new-session
+    #tmux new-session
+    tmux new-session \; source-file ~/.tmux/new-session
   fi
 
   t
